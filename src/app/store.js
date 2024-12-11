271ab22ce60c078a4efa8de05f45e8eaf288c0ff
Eugine Emoyo Okedi <okedieugine@gmail.com>
2024-12-11 20:36:04 +0300

    updated store with reset password reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import forgotPasswordReducer from '../features/forgotPassword/forgotPasswordSlice';
+import resetPasswordReducer from '../features/forgotPassword/resetPasswordSlice';
 
 const store = configureStore({
     reducer: {
         forgotPassword: forgotPasswordReducer,
+        resetPassword: resetPasswordReducer,
     }
 })
